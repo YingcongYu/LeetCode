802736208601ec67b4a37f6063d70b8ccc8fe9d9
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-02-15 15:49:00 -0500

    Create 137. Single Number II - Attempt 2.py

---
+++ Algorithms/137. Single Number II - Attempt 2.py
@@ -0,0 +1,7 @@
+# Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return int((sum(set(nums))*3 - sum(nums))/2)
