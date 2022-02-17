9df10245c2f2dfbe8900495e4f2df57cdf3e9175
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-02-17 15:15:25 -0500

    Create 511. Game Play Analysis I.sql

---
+++ Database/511. Game Play Analysis I.sql
@@ -0,0 +1,6 @@
+---Write an SQL query to report the first login date for each player.
+
+---Return the result table in any order.
+
+SELECT player_id, min(event_date) AS first_login FROM Activity
+GROUP BY player_id
