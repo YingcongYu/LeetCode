fb9c5d1048547744aa9f32a89cae28d4a0891688
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-02-17 14:19:34 -0500

    Create 260. Single Number III - Attempt 2.py

---
+++ Algorithms/260. Single Number III - Attempt 2.py
@@ -0,0 +1,16 @@
+# # Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice.
+# Find the two elements that appear only once. You can return the answer in any order.
+
+# # You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        if n == 2:
+            return nums
+        else:
+            answer = []
+            for i in nums:
+                if nums.count(i) == 1:
+                    answer.append(i)
+            return answer
