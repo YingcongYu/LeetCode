6d24bda28f718b1789a19258f6891b2919e7a019
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-02-22 09:01:45 -0500

    Create 181. Employees Earning More Than Their Managers - Attemp 1.sql

---
+++ Database/181. Employees Earning More Than Their Managers - Attemp 1.sql
@@ -0,0 +1,9 @@
+---Write an SQL query to find the employees who earn more than their managers.
+
+---Return the result table in any order.
+
+# Write your MySQL query statement below
+SELECT b.name AS  Employee FROM Employee a
+INNER JOIN Employee b
+ON a.id = b.managerId
+WHERE a.salary < b.salary
