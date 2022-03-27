15801686575da7857b4adbd82cdb3d53c802fb1f
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-03-27 15:35:57 -0400

    Create 350. Intersection of Two Arrays II.py

---
+++ Algorithms/350. Intersection of Two Arrays II.py
@@ -0,0 +1,17 @@
+# Given two integer arrays nums1 and nums2, return an array of their intersection. 
+# Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersect = []
+        if len(nums1) <= len(nums2):
+            for i in nums1:
+                if i in nums2:
+                    intersect.append(i)
+                    nums2.remove(i)
+        else:
+            for i in nums2:
+                if i in nums1:
+                    intersect.append(i)
+                    nums1.remove(i)
+        return intersect
