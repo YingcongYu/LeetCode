45bbd8e91511bbe684587b4c8d6bde4d2cd2455d
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-04-06 19:12:20 -0400

    Create 74. Search a 2D Matrix - Method 2.py

---
+++ Algorithms/74. Search a 2D Matrix - Method 2.py
@@ -0,0 +1,11 @@
+# Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in zip(*matrix):
+            if target in i:
+                return True
+        return False
