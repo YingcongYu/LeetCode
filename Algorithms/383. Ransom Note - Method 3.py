536560e95fd09c3e84183eeb084fe49457d04392
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-04-11 01:34:02 -0400

    Create 383. Ransom Note - Method 3.py

---
+++ Algorithms/383. Ransom Note - Method 3.py
@@ -0,0 +1,7 @@
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return Counter(ransomNote) == Counter(ransomNote) & Counter(magazine)
