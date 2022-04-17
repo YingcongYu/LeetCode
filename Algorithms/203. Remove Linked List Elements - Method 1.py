f91255209adad99141ab5477b25b8cdc7021b95a
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-04-17 18:27:59 -0400

    Create 203. Remove Linked List Elements - Method 1.py

---
+++ Algorithms/203. Remove Linked List Elements - Method 1.py
@@ -0,0 +1,24 @@
+# Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if head is None:
+            return head
+        dummy = cur = ListNode(0)
+        while head:
+            if head.val == val and head.next is not None:
+                head = head.next
+            elif head.val == val and head.next is None:
+                head = None
+                cur.next = head
+                cur = cur.next
+            else:
+                cur.next = head
+                head = head.next
+                cur = cur.next
+        return dummy.next
