307b5abd5dbbb8b3fad82ee7348dc19decb009c9
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-04-17 22:14:32 -0400

    Create 1445. Apples & Oranges - Method 1 - Simplify.sql

---
+++ Database/1445. Apples & Oranges - Method 1 - Simplify.sql
@@ -0,0 +1,10 @@
+---Write an SQL query to report the difference between the number of apples and oranges sold each day.
+
+---Return the result table ordered by sale_date.
+
+# Write your MySQL query statement below
+SELECT 
+    sale_date, 
+    sum(CASE WHEN fruit = 'apples' THEN sold_num ELSE -sold_num END) AS diff 
+FROM Sales 
+GROUP BY sale_date
