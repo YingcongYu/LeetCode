b6cdfc5b09e08ffe543d196a572c37c7c2a4b98a
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-07-13 11:15:48 -0400

    Create 189. Rotate Array - Method 1.py

---
+++ Algorithms/189. Rotate Array - Method 1.py
@@ -0,0 +1,11 @@
+# Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        while k > 0:
+            nums.insert(0, nums.pop())
+            k -= 1
