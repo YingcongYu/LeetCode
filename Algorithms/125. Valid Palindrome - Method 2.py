ab5d8315a09db319b2d2e8e5aaaa9693ef8ad273
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2022-08-18 22:41:50 -0700

    Create 125. Valid Palindrome - Method 2.py

---
+++ Algorithms/125. Valid Palindrome - Method 2.py
@@ -0,0 +1,20 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+# it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start, end = 0, len(s)-1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+            elif not s[end].isalnum():
+                end -= 1
+            elif s[start].lower() != s[end].lower():
+                return False
+            else:
+                start += 1
+                end -= 1
+        return True
