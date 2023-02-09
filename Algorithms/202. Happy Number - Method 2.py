64dc9352ee30263d69c48489623f7eaf4dcafd7f
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-02-09 14:45:42 -0500

    Create 202. Happy Number - Method 2.py

---
+++ Algorithms/202. Happy Number - Method 2.py
@@ -0,0 +1,21 @@
+# Write an algorithm to determine if a number n is happy.
+
+# A happy number is a number defined by the following process:
+
+# Starting with any positive integer, replace the number by the sum of the squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+# Return true if n is a happy number, and false if not.
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        past = set()
+
+        while n != 1:
+            if n in past:
+                return False
+            else:
+                past.add(n)
+                n = sum([int(x)**2 for x in str(n)])
+        return True
