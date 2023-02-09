ab7dee5342a522a68181c44de694cdaafb1cecd6
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-02-09 16:40:43 -0500

    Create 54. Spiral Matrix - Method 2.py

---
+++ Algorithms/54. Spiral Matrix - Method 2.py
@@ -0,0 +1,6 @@
+# Given an m x n matrix, return all elements of the matrix in spiral order.
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
