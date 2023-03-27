eead8e3fb6c3c528de3db522d05c00c44cfe0948
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-03-27 10:56:12 -0400

    Update 64. Minimum Path Sum - Method 1.py

--- Algorithms/64. Minimum Path Sum - Method 1.py
+++ Algorithms/64. Minimum Path Sum - Method 1.py
@@ -12,12 +12,10 @@ class Solution:
             row.append(sum(grid[-1][i:]))
         for i in range(m-2, -1, -1):
             new_row = grid[i]
-            print(row)
             for j in range(n-1, -1, -1):
                 if j != n-1:
                     new_row[j] += min(row[j], new_row[j+1])
                 else:
                     new_row[j] += row[j]
             row = new_row
-            print(row)
         return row[0]
