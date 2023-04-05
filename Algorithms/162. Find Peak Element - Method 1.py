7e6020d43ec6b745a544db561148fe40ca31b634
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-04-05 11:27:48 -0400

    Create 162. Find Peak Element - Method 1.py

---
+++ Algorithms/162. Find Peak Element - Method 1.py
@@ -0,0 +1,21 @@
+# A peak element is an element that is strictly greater than its neighbors.
+
+# Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+# You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+# You must write an algorithm that runs in O(log n) time.
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid1 = (left+right) // 2
+            mid2 = mid1 + 1
+            if nums[mid1] < nums[mid2]:
+                left = mid2
+            else:
+                right = mid1 
+        return left
