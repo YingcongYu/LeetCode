ac992db5c302f75a85c08e962ac2c73bdfa22bbd
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-04-20 10:43:39 -0400

    Create 662. Maximum Width of Binary Tree - Method 1.py

---
+++ Algorithms/662. Maximum Width of Binary Tree - Method 1.py
@@ -0,0 +1,26 @@
+# Given the root of a binary tree, return the maximum width of the given tree.
+
+# The maximum width of a tree is the maximum width among all levels.
+
+# The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), 
+# where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level are also counted into the length calculation.
+
+# It is guaranteed that the answer will in the range of a 32-bit signed integer.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        ref = collections.defaultdict(list)
+        def dfs(node, level, column):
+            if node:
+                ref[level].append(column)
+                dfs(node.left, level+1, column*2)
+                dfs(node.right, level+1, column*2+1)
+        dfs(root, 0 , 0)
+        return max([max(ref[level]) - min(ref[level]) +1 for level in ref])
