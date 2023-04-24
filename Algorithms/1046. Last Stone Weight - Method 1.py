bab6130e63681e3218c3c8e3faf0e3cfd8bb3c74
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-04-24 10:54:25 -0400

    Create 1046. Last Stone Weight - Method 1.py

---
+++ Algorithms/1046. Last Stone Weight - Method 1.py
@@ -0,0 +1,28 @@
+# You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+# We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. 
+# Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+# If x == y, both stones are destroyed, and
+# If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+# At the end of the game, there is at most one stone left.
+
+# Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        for i, n in enumerate(stones):
+            stones[i] = -n
+        heapq.heapify(stones)
+        
+        while stones:
+            print(stones)
+            y = heapq.heappop(stones)
+            if not stones:
+                return -y
+            x = heapq.heappop(stones)
+            if x != y:
+                heapq.heappush(stones, y-x)
+
+        return 0
