2db3431f3524b018dbfe8154d4f458b2695d9e8a
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-05-01 11:24:47 -0400

    Create 1381. Design a Stack With Increment Operation - Method 2.py

---
+++ Algorithms/1381. Design a Stack With Increment Operation - Method 2.py
@@ -0,0 +1,40 @@
+# Design a stack that supports increment operations on its elements.
+
+# Implement the CustomStack class:
+
+# CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack.
+# void push(int x) Adds x to the top of the stack if the stack has not reached the maxSize.
+# int pop() Pops and returns the top of the stack or -1 if the stack is empty.
+# void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, increment all the elements in the stack.
+
+
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.maxSize = maxSize
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append([x,0])
+
+    def pop(self) -> int:
+        if not self.stack:
+            return -1
+        
+        x, val = self.stack.pop()
+        if self.stack:
+            self.stack[-1][1] += val
+        
+        return x + val
+
+    def increment(self, k: int, val: int) -> None:
+        if self.stack:
+            self.stack[min(k, len(self.stack))-1][1] += val
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
