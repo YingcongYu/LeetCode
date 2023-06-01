96bc80b02dcde9d41c31b52ac34fea3e995fca95
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-06-01 16:29:18 -0400

    Create 706. Design HashMap - Method 1.py

---
+++ Algorithms/706. Design HashMap - Method 1.py
@@ -0,0 +1,30 @@
+# Design a HashMap without using any built-in hash table libraries.
+
+# Implement the MyHashMap class:
+
+# MyHashMap() initializes the object with an empty map.
+# void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+# int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+# void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+
+
+class MyHashMap:
+
+    def __init__(self):
+        self.values = [-1] * (10**6 + 1)
+
+    def put(self, key: int, value: int) -> None:
+        self.values[key] = value
+
+    def get(self, key: int) -> int:
+        return self.values[key]
+
+    def remove(self, key: int) -> None:
+        self.values[key] = -1
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
