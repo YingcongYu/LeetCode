1d5fcb297b0a448ceaa98da83032b4977739f296
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-07-05 14:21:32 -0400

    Create 58. Length of Last Word - Method 1.java

---
+++ Algorithms/58. Length of Last Word - Method 1.java
@@ -0,0 +1,11 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ", 0);
+        return arr[arr.length-1].length();
+    }
+}
