f625d8c2b780714c5d9d1e31efc9eb2ddcf6fa87
YingcongYu <84693018+YingcongYu@users.noreply.github.com>
2023-07-12 12:26:26 -0400

    Create 802. Find Eventual Safe States - Method 1.py

---
+++ Algorithms/802. Find Eventual Safe States - Method 1.py
@@ -0,0 +1,26 @@
+# There is a directed graph of n nodes with each node labeled from 0 to n - 1. 
+# The graph is represented by a 0-indexed 2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, 
+# meaning there is an edge from node i to each node in graph[i].
+
+# A node is a terminal node if there are no outgoing edges. 
+# A node is a safe node if every possible path starting from that node leads to a terminal node (or another safe node).
+
+# Return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
+
+
+import bisect
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        safe = []
+        ref = [False] * len(graph)
+
+        flag = 1
+        while flag > 0:
+            flag = 0
+            for index, go_to in enumerate(graph):
+                if not ref[index] and (len(go_to) == 0 or all(ref[x] for x in go_to)):
+                    bisect.insort(safe, index)
+                    flag += 1
+                    ref[index] = True
+        
+        return safe
